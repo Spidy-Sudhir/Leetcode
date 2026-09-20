733ff47f5b094cce2de21eec805ade46616757c3
Sudhir Kumar Gupta <sudhirgupta9july@gmail.com>
2026-09-20 17:53:53 +0530

    Time: 8 ms (34.96%), Space: 19.3 MB (19.13%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.py
@@ -0,0 +1,17 @@
+# class Solution:
+#     def reverseDegree(self, s: str) -> int:
+#         total = 0
+#         for i in range(len(s)):
+#             val = (ord(s[i]) - ord('a') + 1) * (i+1 )
+#             total += val
+#         return total
+
+class Solution:
+    def reverseDegree(self, s: str) -> int:
+
+        ans, idx = 0, 1
+        for ch in s:
+            ans+= (123 - ord(ch)) * idx
+            idx+= 1
+
+        return ans    
